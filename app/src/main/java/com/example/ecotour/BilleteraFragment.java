de8cb5a3a06d2aad1bb047a845384d714a55b81a
valecruzdp <vcruzd@unal.edu.co>
2021-08-03 16:37:32 -0500

    Se agregó página de programar Viaje e intenté cambiar icono de la app. Enlacé el botón de billetera con la activtity de programar viaje para verificar que funcionaba, pero no será así.

--- app/src/main/java/com/example/ecotour/BilleteraFragment.java
+++ app/src/main/java/com/example/ecotour/BilleteraFragment.java
@@ -1,10 +1,12 @@
 package com.example.ecotour;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
@@ -24,8 +26,10 @@ public class BilleteraFragment extends Fragment implements View.OnClickListener 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View vista = inflater.inflate(R.layout.fragment_billetera, container, false);
+
+        //Seleccionar alguna opcion.
+
         Spinner spinner_noche = (Spinner) vista.findViewById(R.id.spinner_noches);
 
         Spinner spinner_habitacion = (Spinner) vista.findViewById(R.id.spinner_habitaciones);
@@ -45,13 +49,22 @@ public class BilleteraFragment extends Fragment implements View.OnClickListener 
         spinner_noche.setAdapter(adapterNoche);
         spinner_habitacion.setAdapter(adapterHabitacion);
         spinner_personas.setAdapter(adapterPersonas);
-        
+
+        //Boton guardar preferencias.
+
+        Button guardar = (Button) vista.findViewById(R.id.billera_btn);
+        guardar.setOnClickListener(this);
+
         return vista;
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.billera_btn:
+                startActivity(new Intent(getContext(), programarViaje.class));
+                break;
+        }
     }
 
 }
--- app/src/main/java/com/example/ecotour/programarViaje.java
+++ app/src/main/java/com/example/ecotour/programarViaje.java
@@ -2,6 +2,8 @@ package com.example.ecotour;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class programarViaje extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class programarViaje extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programar_viaje);
+
+        Spinner spinner_año = (Spinner) findViewById(R.id.spinner_año);
+        Spinner spinner_mes = (Spinner) findViewById(R.id.spinner_mes);
+        Spinner spinner_dia1 = (Spinner) findViewById(R.id.spinner_dia1);
+        Spinner spinner_dia2 = (Spinner) findViewById(R.id.spinner_dia2);
+
+        ArrayAdapter<String> adapterAño =new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.año));
+        ArrayAdapter<String> adapterMes =new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.mes));
+        ArrayAdapter<String> adapterDia =new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.dia_inicial));
+
+        adapterAño.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_año.setAdapter(adapterAño);
+        adapterMes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_mes.setAdapter(adapterMes);
+        adapterDia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_dia1.setAdapter(adapterDia);
+        spinner_dia2.setAdapter(adapterDia);
+
     }
 }
