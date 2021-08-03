c8f669bc4eceb8126145d9d51e1629ffe49ca62e
valecruzdp <vcruzd@unal.edu.co>
2021-08-03 15:20:21 -0500

    Se agregó página de Calculadora de presupuesto, más el fondo de favoritos y de viajes.

--- app/src/main/java/com/example/ecotour/BilleteraFragment.java
+++ app/src/main/java/com/example/ecotour/BilleteraFragment.java
@@ -4,60 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BilleteraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BilleteraFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class BilleteraFragment extends Fragment implements View.OnClickListener {
 
     public BilleteraFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BilleteraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BilleteraFragment newInstance(String param1, String param2) {
-        BilleteraFragment fragment = new BilleteraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
+
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_billetera, container, false);
+
+        View vista = inflater.inflate(R.layout.fragment_billetera, container, false);
+        Spinner spinner_noche = (Spinner) vista.findViewById(R.id.spinner_noches);
+
+        Spinner spinner_habitacion = (Spinner) vista.findViewById(R.id.spinner_habitaciones);
+
+        Spinner spinner_personas = (Spinner) vista.findViewById(R.id.spinner_personas);
+
+        ArrayAdapter<String> adapterNoche =new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.noches));
+
+        ArrayAdapter<String> adapterHabitacion =new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.habitaciones));
+
+        ArrayAdapter<String> adapterPersonas =new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.personas));
+
+        adapterNoche.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_noche.setAdapter(adapterNoche);
+        spinner_habitacion.setAdapter(adapterHabitacion);
+        spinner_personas.setAdapter(adapterPersonas);
+        
+        return vista;
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
---
+++ app/src/main/java/com/example/ecotour/programarViaje.java
@@ -0,0 +1,13 @@
+package com.example.ecotour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class programarViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programar_viaje);
+    }
+}
