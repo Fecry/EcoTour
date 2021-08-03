8b73dc200bc4e8b6f3c438db6daae94af729063e
valecruzdp <vcruzd@unal.edu.co>
2021-08-03 16:47:24 -0500

    Aparece mensaje cuando se oprime botón de programarViaje

--- app/src/main/java/com/example/ecotour/programarViaje.java
+++ app/src/main/java/com/example/ecotour/programarViaje.java
@@ -2,15 +2,24 @@ package com.example.ecotour;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class programarViaje extends AppCompatActivity {
+public class programarViaje extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_programar_viaje);
+
+        //Boton
+        Button guardar = findViewById(R.id.programar_btn);
+        guardar.setOnClickListener(this);
+
+        //Spinner, seleccionar opciones
 
         Spinner spinner_año = (Spinner) findViewById(R.id.spinner_año);
         Spinner spinner_mes = (Spinner) findViewById(R.id.spinner_mes);
@@ -32,5 +41,12 @@ public class programarViaje extends AppCompatActivity {
         spinner_dia1.setAdapter(adapterDia);
         spinner_dia2.setAdapter(adapterDia);
 
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.programar_btn:
+                Toast.makeText(programarViaje.this, "Se ha programado su viaje exitosamente.", Toast.LENGTH_LONG).show();
+        }
     }
 }
