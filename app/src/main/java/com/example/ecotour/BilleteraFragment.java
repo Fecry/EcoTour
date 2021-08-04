9116dda4e27ae10d3a4be0e84ba7222b7fb928b5
valecruzdp <vcruzd@unal.edu.co>
2021-08-03 22:21:33 -0500

    "Guardé" las opciones que el usuario selecciona en un Array. Sin embargo, se imprime correctamente el Array pero no funciona del todo bien porque no aparecen los datos seleccionados guardados :(.

--- app/src/main/java/com/example/ecotour/BilleteraFragment.java
+++ app/src/main/java/com/example/ecotour/BilleteraFragment.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class BilleteraFragment extends Fragment implements View.OnClickListener {
-
+    String[] opcionesArray = {"Noches", "", "Habitaciones", "", "Personas", ""};
+    String opciones= "";
     public BilleteraFragment() {
         // Required empty public constructor
     }
@@ -29,31 +32,67 @@ public class BilleteraFragment extends Fragment implements View.OnClickListener 
         View vista = inflater.inflate(R.layout.fragment_billetera, container, false);
 
         //Seleccionar alguna opcion.
-
         Spinner spinner_noche = (Spinner) vista.findViewById(R.id.spinner_noches);
-
         Spinner spinner_habitacion = (Spinner) vista.findViewById(R.id.spinner_habitaciones);
-
         Spinner spinner_personas = (Spinner) vista.findViewById(R.id.spinner_personas);
 
-        ArrayAdapter<String> adapterNoche =new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.noches));
+        ArrayAdapter<CharSequence> adapterNoche =ArrayAdapter.createFromResource(getActivity(),R.array.noches,
+                android.R.layout.simple_list_item_1);
 
-        ArrayAdapter<String> adapterHabitacion =new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.habitaciones));
+        ArrayAdapter<CharSequence> adapterHabitacion = ArrayAdapter.createFromResource(getActivity(),R.array.habitaciones,
+                android.R.layout.simple_list_item_1);
 
-        ArrayAdapter<String> adapterPersonas =new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.personas));
+        ArrayAdapter<CharSequence> adapterPersonas =ArrayAdapter.createFromResource(getActivity(),R.array.personas,
+                android.R.layout.simple_list_item_1);
 
-        adapterNoche.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_noche.setAdapter(adapterNoche);
         spinner_habitacion.setAdapter(adapterHabitacion);
         spinner_personas.setAdapter(adapterPersonas);
 
-        //Boton guardar preferencias.
+        //Boton
 
-        Button guardar = (Button) vista.findViewById(R.id.billera_btn);
+        Button guardar = vista.findViewById(R.id.billera_btn);
         guardar.setOnClickListener(this);
+
+        //Obtener elemento cantidad noches
+        spinner_noche.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String noches = parent.getItemAtPosition(position).toString();
+                opcionesArray[1]= noches;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        //Obtener elemento cantidad habitaciones
+        spinner_habitacion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String habitaciones = parent.getItemAtPosition(position).toString();
+                opcionesArray[3]=habitaciones;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        //Obtener elemento cantidad personas
+        spinner_personas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String personas = parent.getItemAtPosition(position).toString();
+                opcionesArray[5]= personas;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        for(int i=0; i<5; i++){
+            opciones= opciones+opcionesArray[i];
+            if(i%2==0){
+                opciones = opciones + ": ";
+            }
+        }
 
         return vista;
     }
@@ -62,6 +101,8 @@ public class BilleteraFragment extends Fragment implements View.OnClickListener 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.billera_btn:
+                Toast.makeText(getContext(), "Procesando... ", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), opciones, Toast.LENGTH_LONG).show();
                 startActivity(new Intent(getContext(), programarViaje.class));
                 break;
         }
--- app/src/main/java/com/example/ecotour/programarViaje.java
+++ app/src/main/java/com/example/ecotour/programarViaje.java
@@ -3,13 +3,16 @@ package com.example.ecotour;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class programarViaje extends AppCompatActivity implements View.OnClickListener {
-
+    String[] opcionesArray = {"Año", "", "Mes","", "Dia inicio", "", "Dia final", ""};
+    String opciones = "";
+    String opciones_prueba = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,27 +29,80 @@ public class programarViaje extends AppCompatActivity implements View.OnClickLis
         Spinner spinner_dia1 = (Spinner) findViewById(R.id.spinner_dia1);
         Spinner spinner_dia2 = (Spinner) findViewById(R.id.spinner_dia2);
 
-        ArrayAdapter<String> adapterAño =new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.año));
-        ArrayAdapter<String> adapterMes =new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.mes));
-        ArrayAdapter<String> adapterDia =new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.dia_inicial));
+        ArrayAdapter<CharSequence> adapterAño = ArrayAdapter.createFromResource(this,R.array.año,
+                android.R.layout.simple_list_item_1);
+        ArrayAdapter<CharSequence> adapterMes =ArrayAdapter.createFromResource(this,R.array.mes,
+                android.R.layout.simple_list_item_1);
+        ArrayAdapter<CharSequence> adapterDia = ArrayAdapter.createFromResource(this, R.array.dia_inicial,
+                android.R.layout.simple_list_item_1);
 
-        adapterAño.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_año.setAdapter(adapterAño);
-        adapterMes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_mes.setAdapter(adapterMes);
-        adapterDia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_dia1.setAdapter(adapterDia);
         spinner_dia2.setAdapter(adapterDia);
 
+        //Obtener elemento de Año
+        spinner_año.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String año = parent.getItemAtPosition(position).toString();
+                opcionesArray[1] = año;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Obtener elemento de Mes
+        spinner_mes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String mes = parent.getItemAtPosition(position).toString();
+                opcionesArray[3] = mes;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        //Obtener elemento de Dia en que empieza
+        spinner_dia1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String dia1 = parent.getItemAtPosition(position).toString();
+                opcionesArray[5] = dia1;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        //Obtener elemento de Dia en que termina
+        spinner_dia2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String dia2 = parent.getItemAtPosition(position).toString();
+                opcionesArray[7] = dia2;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //probar si está funcionando el Array porque supongo que la función se puede desarrollar más fácil con un Array.
+        for(int i=0; i<7; i++){
+            opciones= opciones+opcionesArray[i];
+            if(i%2==0){
+                opciones = opciones + ": ";
+            }
+        }
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.programar_btn:
-                Toast.makeText(programarViaje.this, "Se ha programado su viaje exitosamente.", Toast.LENGTH_LONG).show();
+                Toast.makeText(programarViaje.this, "Viaje programado. ", Toast.LENGTH_LONG).show();
+                Toast.makeText(programarViaje.this, opciones, Toast.LENGTH_LONG).show();
+                break;
         }
     }
 }
